8ab8c227d3e1275bfd1a039ea05990f32c0093da
Elyas Demeke <nebiyou.elyas11@gmail.com>
2020-12-27 16:03:53 +0300

    first save

---
+++ polls/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('I will be a better developer in python and django with in weeks')
